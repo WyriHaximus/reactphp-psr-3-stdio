4987926e9dc306485ff3434948dc94da77695b88
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-12-27 02:46:34 +0100

    Updated to new CS rules

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,6 @@
 
 use PHPUnit\Framework\TestCase;
 
-require \dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require \dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php';
 
 \class_alias(TestCase::class, '\PHPUnit_Framework_TestCase');
